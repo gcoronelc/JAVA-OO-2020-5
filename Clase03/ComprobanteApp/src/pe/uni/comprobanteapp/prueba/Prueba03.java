dc7f202b5071ca872575381ee9a94f2987f44819
Gustavo Coronel <gcoronelc@gmail.com>
2020-02-29 19:17:49 -0500

    Clase 03

---
+++ Clase03/ComprobanteApp/src/pe/uni/comprobanteapp/prueba/Prueba03.java
@@ -0,0 +1,28 @@
+package pe.uni.comprobanteapp.prueba;
+
+import pe.uni.comprobanteapp.model.ConceptoModel;
+import pe.uni.comprobanteapp.service.CompAbstract;
+import pe.uni.comprobanteapp.service.CompFactory;
+
+/**
+ *
+ * @author Gustavo Coronel
+ * @blog   http://gcoronelc.blogspot.com/
+ * @youtube https://www.youtube.com/c/DesarrollaSoftware
+ */
+public class Prueba03 {
+
+	public static void main(String[] args) {
+		// Dato
+		double total = 3456.76;
+		// Proceso
+		CompAbstract service;
+		service = CompFactory.getComp(CompFactory.TIPO_BOLETA);
+		ConceptoModel[] repo = service.proceso(total);
+		// Reporte
+		for (ConceptoModel bean : repo) {
+			System.out.println(bean);
+		}
+	}
+	
+}
---
+++ Clase03/ComprobanteApp/src/pe/uni/comprobanteapp/service/CompFactory.java
@@ -0,0 +1,32 @@
+package pe.uni.comprobanteapp.service;
+
+/**
+ *
+ * @author Gustavo Coronel
+ * @blog   http://gcoronelc.blogspot.com/
+ * @youtube https://www.youtube.com/c/DesarrollaSoftware
+ */
+public class CompFactory {
+
+	// Tipos de Comprobantes
+	public static final String TIPO_FACTURA = "FACTURA";
+	public static final String TIPO_BOLETA = "BOLETA";
+	
+	
+	private CompFactory() {
+	}
+
+	public static CompAbstract getComp(String tipo){
+		CompAbstract comp = null;
+		switch(tipo){
+			case TIPO_FACTURA:
+				comp =  new FacturaService();
+				break;
+			case TIPO_BOLETA:
+				comp =  new BoletaService();
+				break;
+		}
+		return comp;
+	}
+	
+}
